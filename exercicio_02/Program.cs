e079f97aaf649b7a544d275c28e0f703efb140f2
Rafael Pavão <93341589+rafaelpavao@users.noreply.github.com>
2023-05-08 08:58:33 -1100

    Add files via upload

---
+++ exercicio_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+float tempF, tempC;
+bool flag;
+
+float conversion(float tempC)
+{
+    return (9 * tempC + 160) / 5;
+}
+
+do{
+    Console.WriteLine("Digite a temperatura em graus Celsius: ");
+    flag = float.TryParse(Console.ReadLine().Replace(".", ","), out tempC);
+    if (!flag){
+        Console.WriteLine("Formato inválido, digite apenas números"); 
+    }
+} while (!flag);
+
+tempF = conversion(tempC);
+
+Console.WriteLine("A temperatura em Fahrenheit é: " + tempF);
